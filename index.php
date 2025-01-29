50fc24ba868ad6a27bcf05b393c7834dba25de71
PhirabRIN <150504821+PhirabRIN@users.noreply.github.com>
2025-01-29 16:01:26 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OZZY-Online Store</title>
+    <link rel="icon" href="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQnaOTbfJDrWrVHc3g5C69izw2PFpaMY3LO2qG4VTK1qmQdsIRcsW0ucfFDop6wKTKjRA&usqp=CAU">
+</head>
+<body>
+    <a href="Admin/login.php">Login</a>
+</body>
+</html>
